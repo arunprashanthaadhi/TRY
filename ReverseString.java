741f76373347bf34878ac4d6f95016950abcef71
arunprashanthaadhi <prashanth.arun1234@gmail.com>
2024-03-13 17:27:44 +0530

    Puni

--- ReverseString.java
+++ ReverseString.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class ReverseString {
     public static void main(String[] args) {
-        String originalString = "Hello, World!";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value which you want to reverse: ");
+        String originalString = scanner.nextLine();
         String reversedString = new StringBuilder(originalString).reverse().toString();
         System.out.println("Reversed string: " + reversedString);
     }
